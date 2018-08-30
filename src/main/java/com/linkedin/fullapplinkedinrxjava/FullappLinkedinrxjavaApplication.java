63b37d9af6c935d537134fe9a34ba8399170b4de
Pawel Dobrzanski <pawel.dobrzanski.ctr@sabre.com>
2018-08-30 17:58:18 +0200

    setting up spring boot reactive project

---
+++ src/main/java/com/linkedin/fullapplinkedinrxjava/FullappLinkedinrxjavaApplication.java
@@ -0,0 +1,12 @@
+package com.linkedin.fullapplinkedinrxjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullappLinkedinrxjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullappLinkedinrxjavaApplication.class, args);
+	}
+}
